8145e643c136e90ffa6db0b15f0bbec6bc0b1c6b
Neal Fultz <nfultz@gmail.com>
2017-06-10 10:54:33 -0700

    Adding AWS batch fields

--- src/main/java/njnm/plugins/hello_world/HelloWorldBuilder.java
+++ src/main/java/njnm/plugins/hello_world/HelloWorldBuilder.java
@@ -12,7 +12,12 @@ import org.kohsuke.stapler.DataBoundConstructor;
 
 import javax.management.Descriptor;
 
+//import com.amazonaws.services.batch.AWSBatchClientBuilder;
+
+
 import net.sf.json.JSONObject;
+
+import java.util.HashMap;
 
 /**
  * Sample {@link Builder}.
@@ -22,7 +27,7 @@ import net.sf.json.JSONObject;
  * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
  * and a new {@link HelloWorldBuilder} is created. The created
  * instance is persisted to the project configuration XML by using
- * XStream, so this allows you to use instance fields (like {@link #name})
+ * XStream, so this allows you to use instance fields (like {@link #command})
  * to remember the configuration.
  *
  * <p>
@@ -33,36 +38,64 @@ import net.sf.json.JSONObject;
  */
 public class HelloWorldBuilder extends Builder {
 
-    private final String name;
+    private final  String jobname;
+    private final String jobdefinition;
+    private final String jobqueue;
+    private final  String command;
+    private final  String vcpu;
+    private final  String memory;
+    private final  String retries;
+    private final HashMap<String, String> params;
+    private final HashMap<String, String> environment;
+
 
     /**
      * This annotation tells Hudson to call this constructor, with
      * values from the configuration form page with matching parameter names.
      */
     @DataBoundConstructor
-    public HelloWorldBuilder(String name) {
-        this.name = name;
+    public HelloWorldBuilder(String jobname, String jobdefinition,
+                             String command, String jobqueue, String vcpu,
+                             String memory,  String retries,
+                             HashMap<String, String> params,
+                             HashMap<String, String> environment) {
+        this.jobname = jobname;
+        this.jobdefinition = jobdefinition;
+        this.jobqueue = jobqueue;
+        this.command = command;
+        this.vcpu = vcpu;
+        this.memory = memory;
+        this.retries = retries;
+        this.params = params;
+        this.environment = environment;
     }
 
     /**
      * We'll use this from the <tt>config.jelly</tt>.
      */
-    public String getName() {
-        return name;
+
+    public String getJobdefinition() {
+        return jobdefinition;
     }
+    public String getCommand()       { return command;}
+    public String getJobname()       { return jobname; }
+    public String getJobqueue()      { return jobqueue; }
+    public String getVcpu()          { return vcpu; }
+    public String getMemory()        { return memory; }
+    public String getRetries()       { return retries; }
 
     @Override
     public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
-        // this is where you 'build' the project
+        // this is where you 'build' the projectx
         // since this is a dummy, we just say 'hello world' and call that a build
 
         // this also shows how you can consult the global configuration of the builder
-        if(getDescriptor().useFrench())
-            listener.getLogger().println("Bonjour, "+name+"!");
-        else
-            listener.getLogger().println("Hello, "+name+"!");
-        return true;
+            listener.getLogger().println(getDescriptor().toString());
+            listener.getLogger().println(this.toString());
+            return true;
     }
+
+
 
     /**
      * Hudson defines a method {@link Builder#getDescriptor()}, which
@@ -98,7 +131,9 @@ public class HelloWorldBuilder extends Builder {
          * <p>
          * If you don't want fields to be persisted, use <tt>transient</tt>.
          */
-        private boolean useFrench;
+        private String awsAccessKey;
+        private String awsSecretToken;
+        private String awsRegion;
 
         public DescriptorImpl() {
             load();
@@ -124,7 +159,10 @@ public class HelloWorldBuilder extends Builder {
         public boolean configure(StaplerRequest staplerRequest, JSONObject json) throws FormException {
             // to persist global configuration information,
             // set that to properties and call save().
-            useFrench = json.getBoolean("useFrench");
+            awsAccessKey   = json.getString("awsAccessKey");
+            awsSecretToken = json.getString("awsSecretToken");
+            awsRegion      = json.getString("awsRegion");
+
             save();
             return true; // indicate that everything is good so far
         }
@@ -132,8 +170,19 @@ public class HelloWorldBuilder extends Builder {
         /**
          * This method returns true if the global configuration says we should speak French.
          */
-        public boolean useFrench() {
-            return useFrench;
+        public String getAwsAccessKey()   { return awsAccessKey; }
+        public String getAwsSecretToken() {
+            return awsSecretToken;
+        }
+        public String getAwsRegion()      { return awsRegion ; }
+
+        @Override
+        public String toString() {
+            return "DescriptorImpl{" +
+                    "awsAccessKey='" + awsAccessKey + '\'' +
+                    ", awsSecretToken='" + awsSecretToken + '\'' +
+                    ", awsRegion='" + awsRegion + '\'' +
+                    '}';
         }
     }
 }
